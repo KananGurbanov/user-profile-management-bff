860d581de62fb6e3944707cdd43e9f7c677dbb15
HP ProBook <kalbaliyevaselen@gmai.com>
2024-08-15 18:01:11 +0400

    initial commit

--- src/main/java/az/edu/turing/userprofilemanagementbff/handler/FeignErrorDecoder.java
+++ src/main/java/az/edu/turing/userprofilemanagementbff/handler/FeignErrorDecoder.java
@@ -1,5 +1,20 @@
 package az.edu.turing.userprofilemanagementbff.handler;
 
-public class FeignErrorDecoder {
+import az.edu.turing.userprofilemanagementbff.exceptions.BadRequestException;
+import az.edu.turing.userprofilemanagementbff.exceptions.RecordNotFoundException;
+import feign.Response;
+import feign.codec.ErrorDecoder;
 
+public class FeignErrorDecoder implements ErrorDecoder {
+    @Override
+    public Exception decode(String s, Response response) {
+        switch (response.status()) {
+            case 400:
+                return new BadRequestException("400","Bad Request");
+            case 404:
+                return new RecordNotFoundException("404","Record not found");
+            default:
+                return new Exception("Unknown error occurred. Status code: " + response.status());
+        }
+    }
 }
